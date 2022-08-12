d7498074de5b982066e3d5ee4086bb2d2911df23
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-12 19:24:12 +0300

    Task 10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+     Console.WriteLine("Введите 3-х число: ");
+     string? Num = Console.ReadLine();
+     Console.WriteLine("Вторая цифра этого числа: " + Num![1]);
+    }
+}
