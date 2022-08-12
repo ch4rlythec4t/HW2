6847ce738a8ec0d20faaf6359dc98e9085efc288
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-12 19:58:09 +0300

    Task 13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+        string? Num = Console.ReadLine();
+        int NumLenght = Num!.Length;
+
+        if (NumLenght < 3)
+        {
+            Console.WriteLine ("3-й цифры нет");
+        }
+        else
+        {
+          Console.WriteLine ("3-я цифра введенного числа: " + Num![2]);
+        }
+
+    }
+}
