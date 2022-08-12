d41c54413dd5bdd0dd896b052b52e1ee2348800b
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-12 20:01:14 +0300

    bagfix

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main()
     {
-     Console.WriteLine("Введите 3-х число: ");
+     Console.WriteLine("Введите 3-хзначное число: ");
      string? Num = Console.ReadLine();
-     Console.WriteLine("Вторая цифра этого числа: " + Num![1]);
+     Console.WriteLine("Вторая цифра введенного числа: " + Num![1]);
     }
 }
